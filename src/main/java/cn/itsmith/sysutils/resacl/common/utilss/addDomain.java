91266ae028d8023f6eeca5b10c3fe71b12dfd787
Your Name <email@example.com>
2019-12-02 17:07:27 +0800

    增加name，字段，修改

--- src/main/java/cn/itsmith/sysutils/resacl/common/utilss/addDomain.java
+++ src/main/java/cn/itsmith/sysutils/resacl/common/utilss/addDomain.java
@@ -7,4 +7,20 @@ import lombok.Data;
 public class addDomain {
     String domDes;
     String domName;
+
+    public String getDomName() {
+        return domName;
+    }
+
+    public void setDomName(String domName) {
+        this.domName = domName;
+    }
+
+    public String getDomDes() {
+        return domDes;
+    }
+
+    public void setDomDes(String domDes) {
+        this.domDes = domDes;
+    }
 }
