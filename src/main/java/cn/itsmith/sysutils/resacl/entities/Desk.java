3e136c24db18ad775d73268b03e100520457725a
xiexie2917 <xiexie2917@users.noreply.github.com>
2019-12-05 16:53:05 +0800

    添加的桌子

---
+++ src/main/java/cn/itsmith/sysutils/resacl/entities/Desk.java
@@ -0,0 +1,11 @@
+package cn.itsmith.sysutils.resacl.entities;
+
+import lombok.Data;
+
+@Data
+public class Desk {
+    Integer resId;
+    String name;
+    String description;
+    String shape;
+}
