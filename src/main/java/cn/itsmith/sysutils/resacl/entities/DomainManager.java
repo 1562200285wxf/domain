0c189526335cfd5281ee6fb486872110de373e42
Your Name <email@example.com>
2019-12-26 17:07:26 +0800

    管理人员

---
+++ src/main/java/cn/itsmith/sysutils/resacl/entities/DomainManager.java
@@ -0,0 +1,14 @@
+package cn.itsmith.sysutils.resacl.entities;
+
+import lombok.Data;
+
+//域，
+@Data
+public class DomainManager {
+    Integer domId;
+    Integer domManagerId;
+    String didomManagerTel;
+    String domToken;
+    String name;
+    Integer id;
+}
